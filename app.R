eb690a1a88a4288e1adcf0edd424dd55fa97a56c
Uche Kalu <102996478+UchePat@users.noreply.github.com>
2022-04-28 15:59:58 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -1,11 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-
 #  Esquisse ggplot builder --- We build ggplot charts with drag-and-drop (just like in Tableau software) using esquisse package
 # Esquisse package is used to build all ggplot charts without writing a single ggplot code
 
@@ -15,53 +7,53 @@ library(ggplot2)  # containing d mpg dataset
 library(esquisse)
 library(modeldata)
 library(shiny)
-library(rsconnect)
+library(shinythemes)
+#library(rsconnect)
 
 # Datasets
 data("drinks")
 data("mpg")
 
-
-# U.I part
-ui <- navbarPage(title = h3("Use Esquisse as a Shiny module"),   # creates a menu with d stated title
-                 
-                 theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
+ui <- fluidPage(
     
-    sidebarLayout(   # creates a sidebar
-        sidebarPanel(
-            radioButtons(   # radioButtons() will create radio buttons for the parameters
-                inputId = "data",
-                label = h4("Data to use:"),
-                choices = c("drinks", "mpg"),
-                inline = TRUE
-            ),
-            
-        # using CSS to style d app
-            tags$style("body{background-color:linen; color:brown}"),
-            tags$style(".span12{font-style: oblique;border-style: solid}"),
-            tags$style(".span4{border-style: solid}"),
-            tags$style(".span8{border-style: solid}")
-        ),
-        
-        mainPanel(
-            tabsetPanel(     # creates tab page column layout in d main tab(not in d menu) 
-                tabPanel(title = "esquisse",   # dis is 1st tab page  with d stated title
-                    esquisserUI(
-                        id = "esquisse",    # using esquisse ggplot builder package
-                        header = FALSE, # dont display gadget title
-                        choose_data = FALSE # dont display button to change data
-                    )
-                ),
-                tabPanel(title = "output",       # dis is 2nd tab page with d stated title
-                    verbatimTextOutput("module_out")  # dis object is created in Server function part
-                )
-            )
-        )
+    theme=shinytheme("slate"),   # using slate shinytheme
+    
+    # themeSelector(), ## displays the various themes options you can pick from
+    
+    navbarPage(
+        title ="Using Esquisse package for ggplot Visualization",
+        id ="nav",
+
+             sidebarLayout(   # creates a sidebar
+                     sidebarPanel(
+                         radioButtons(   # radioButtons() will create radio buttons for the parameters
+                             inputId = "data",
+                             label = "Select any Dataset:",
+                             choices = c("drinks", "mpg"),
+                             inline = TRUE
+                         ),
+                         
+                  ),
+                     
+                     mainPanel(
+                         tabsetPanel(     # creates tab page column layout in d main tab(not in d menu) 
+                             tabPanel(title = "esquisse",   # dis is 1st tab page  with d stated title
+                                      esquisserUI(
+                                          id = "esquisse",    # using esquisse ggplot builder package
+                                          header = FALSE, # dont display gadget title
+                                          choose_data = FALSE # dont display button to change data
+                                      )
+                             ),
+                             tabPanel(title = "output",       # dis is 2nd tab page with d stated title
+                                      verbatimTextOutput("module_out")  # dis object is created in Server function part
+                             )
+                         )
+                     )
+                 )
     )
-)
+)    
 
 
-# Server function part
 server <- function(input, output, session) {
     
     data_r <- reactiveValues(data = drinks, name = "drinks")  # using drinks dataset
